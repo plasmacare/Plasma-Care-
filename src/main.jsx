5de26045784fd9f9f5a2f3cf3c17de6e00af891e
plasmacare <plasmacaretechnicaluse@gmail.com>
2026-08-05 14:02:15 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
+import { LanguageProvider } from './lib/i18n.jsx'
 import './styles/tokens.css'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <LanguageProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </LanguageProvider>
   </React.StrictMode>
 )
