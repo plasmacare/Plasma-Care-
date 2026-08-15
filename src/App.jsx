66349b27817a480df65d35de1dc3954e4b10af60
plasmacare <plasmacaretechnicaluse@gmail.com>
2026-08-15 22:34:54 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import PathologyBooking from './pages/PathologyBooking'
-import Account from './pages/Account'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/book/pathology" element={<PathologyBooking />} />
-      <Route path="/account" element={<Account />} />
     </Routes>
   )
 }
