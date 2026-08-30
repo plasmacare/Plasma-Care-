b24a930ba5962f7fbc89f9e07f3ec9fe8540f2d9
plasmacare <plasmacaretechnicaluse@gmail.com>
2026-08-30 09:11:53 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import PathologyBooking from './pages/PathologyBooking'
 import LegalPage from './pages/LegalPage'
 import PaymentStatus from './pages/PaymentStatus'
 import SiteBackground from './components/SiteBackground'
+import Analytics from './components/Analytics'
 
 export default function App() {
   return (
     <>
       <SiteBackground />
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/book/pathology" element={<PathologyBooking />} />
